110a917012407cddf07c5d2db8916693c1425965
Nisay <110921275+YasinDehfuli@users.noreply.github.com>
2023-05-23 19:10:58 +0330

    Add table use (1.php artisan make:migrate, 2.php artisan make:model Post -m, 3.php artisan migrate:fresh)

---
+++ database/migrations/2023_05_23_152541_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('body');
+            $table->string('image');
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
